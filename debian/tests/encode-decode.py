4371f5c3c120f5c13ae75dc049bd5eadbd37c0a7
Ondřej Nový <novy@ondrej.org>
2016-03-01 22:53:02 +0100

    Tests liberasure backends in Debian tests.

--- debian/tests/encode-decode.py
+++ debian/tests/encode-decode.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python3
-
 from pyeclib.ec_iface import ECDriver
+import sys
 
 input = b'test'
 
-ec = ECDriver(k=2, m=1, ec_type='liberasurecode_rs_vand')
+ec = ECDriver(k=2, m=1, ec_type=sys.argv[1])
 fragments = ec.encode(input)
 assert ec.decode(fragments[0:2]) == input
-
