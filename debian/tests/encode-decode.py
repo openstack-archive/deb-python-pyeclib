8b3815a7d9710ddbb3b01d2fcd11a82781423c9f
Ondřej Nový <novy@ondrej.org>
2016-06-02 12:48:53 +0200

    Print debug info in Debian test

--- debian/tests/encode-decode.py
+++ debian/tests/encode-decode.py
@@ -1,8 +1,20 @@
+from __future__ import print_function
 from pyeclib.ec_iface import ECDriver
 import sys
 
 input = b'test'
 
+# Init
+print("init:", end=" ")
 ec = ECDriver(k=3, m=3, hd=3, ec_type=sys.argv[1])
+print("OK")
+
+# Encode
+print("encode:", end=" ")
 fragments = ec.encode(input)
+print("OK")
+
+# Decode
+print("decode:", end=" ")
 assert ec.decode(fragments[0:ec.k]) == input
+print("OK")
